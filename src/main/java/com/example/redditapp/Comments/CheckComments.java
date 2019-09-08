e718958a84db99cf1514d6026479e5698ae4b1df
Ameya Deshmukh <ameyand.98@gmail.com>
2019-09-08 18:50:24 -0500

    Create CheckComments.java

---
+++ src/main/java/com/example/redditapp/Comments/CheckComments.java
@@ -0,0 +1,19 @@
+package com.example.redditapp.Comments;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class CheckComment {
+
+    @SerializedName("success")
+    @Expose
+    private String success;
+
+    public String getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(String success) {
+        this.success = success;
+    }
+}
