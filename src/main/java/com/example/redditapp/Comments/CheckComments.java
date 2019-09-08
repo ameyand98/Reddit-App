f2cc7fdb9177a0a5689d0341e3e6ef468265dd4b
Ameya Deshmukh <ameyand.98@gmail.com>
2019-09-08 18:50:55 -0500

    Update CheckComments.java

--- src/main/java/com/example/redditapp/Comments/CheckComments.java
+++ src/main/java/com/example/redditapp/Comments/CheckComments.java
@@ -3,7 +3,7 @@ package com.example.redditapp.Comments;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-public class CheckComment {
+public class CheckComments {
 
     @SerializedName("success")
     @Expose
