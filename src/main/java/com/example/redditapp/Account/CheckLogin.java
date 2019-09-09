93e6fe186a4184c484a22bd7baa9a4b7f7a4dc25
Ameya Deshmukh <ameyand.98@gmail.com>
2019-09-08 19:05:33 -0500

    Create CheckLogin.java

---
+++ src/main/java/com/example/redditapp/Account/CheckLogin.java
@@ -0,0 +1,20 @@
+package com.example.redditapp.Account;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class CheckLogin {
+
+    @SerializedName("json")
+    @Expose
+    private Json json;
+
+    public Json getJson() {
+        return json;
+    }
+
+    public void setJson(Json json) {
+        this.json = json;
+    }
+
+}
